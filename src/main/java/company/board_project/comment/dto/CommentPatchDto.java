6a2c0f0dc285d9ec2c66f99b7f954b45fe5fad17
kangharyeom <kangharyeom@github.com>
2023-08-14 17:18:11 +0900

    Feat: CommentPatchDto

---
+++ src/main/java/company/board_project/comment/dto/CommentPatchDto.java
@@ -0,0 +1,17 @@
+package company.board_project.comment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class CommentPatchDto {
+    private Long commentId;
+    @NotBlank(message = "댓글의 내용을 입력해야 합니다.")
+    private String comment;
+    private LocalDateTime modifiedAt;
+}
