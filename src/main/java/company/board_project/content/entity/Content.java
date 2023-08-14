1db46c4b1e96c89aed80ebb4324ed88322a8a70a
kangharyeom <kangharyeom@github.com>
2023-08-14 17:00:52 +0900

    Feat: Content

--- src/main/java/company/board_project/content/entity/Content.java
+++ src/main/java/company/board_project/content/entity/Content.java
@@ -1,4 +1,40 @@
 package company.board_project.content.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "CONTENTS")
 public class Content {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CONTENT_ID")
+    private Long contentId;
+
+    // 게시글 제목
+    @Column(nullable = false)
+    private String title;
+
+    // 게시글 내용
+    @Column(nullable = false)
+    private String content;
+
+    // 게시글 작성 시간
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // 게시글 수정 시간
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt;
 }
