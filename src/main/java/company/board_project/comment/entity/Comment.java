e8b8692971b70717868a6bb1a1dbdd5d69bd3671
kangharyeom <kangharyeom@github.com>
2023-08-14 17:17:49 +0900

    Feat: Comment

--- src/main/java/company/board_project/comment/entity/Comment.java
+++ src/main/java/company/board_project/comment/entity/Comment.java
@@ -1,4 +1,35 @@
 package company.board_project.comment.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "COMMENTS")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    // 댓글 내용
+    @Column(nullable = false)
+    private String comment;
+
+    // 댓글 작성 시간
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // 댓글 수정 시간
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt;
 }
