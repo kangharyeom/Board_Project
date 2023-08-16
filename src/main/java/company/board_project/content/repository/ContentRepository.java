e73094527a5d566aa458ca7ca8147f03320edc5e
kangharyeom <kangharyeom@github.com>
2023-08-16 15:06:52 +0900

    Refactor: ContentRepository 기본 쿼리 추가
    - 전체 조회
    - contentId 단위 조회
    - 검색 키워드 단위 조회

--- src/main/java/company/board_project/content/repository/ContentRepository.java
+++ src/main/java/company/board_project/content/repository/ContentRepository.java
@@ -1,4 +1,20 @@
 package company.board_project.content.repository;
 
-public class ContentRepository {
+import company.board_project.content.entity.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ContentRepository extends JpaRepository<Content, Long> {
+    List<Content> findAll();
+    @Query(value = "select * from contents where user_id = :userId", nativeQuery = true)
+    List<Content> findByUserId(@Param("userId") long userId);
+    Optional<Content> findByContentId(long contentId);
+
+    // 게시글 검색기능
+    @Query(value = "select * from contents where title like %:keyword% or content like %:keyword% ", nativeQuery = true)
+    List<Content> findAllSearch(@Param(value = "keyword")String keyword);
 }
