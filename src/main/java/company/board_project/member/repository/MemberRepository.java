dcfa389b1553be121b793c4c7819f819b66f7955
kangharyeom <kangharyeom@github.com>
2023-08-16 12:04:36 +0900

    Refactor: MemberRepository 코드 개선

--- src/main/java/company/board_project/member/repository/MemberRepository.java
+++ src/main/java/company/board_project/member/repository/MemberRepository.java
@@ -2,10 +2,12 @@ package company.board_project.member.repository;
 
 import company.board_project.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
