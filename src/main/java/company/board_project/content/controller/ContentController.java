d618f10269e83fe85b8235f504aae6ec6671affd
kangharyeom <kangharyeom@github.com>
2023-08-16 15:07:41 +0900

    Refactor: ContentController CRUD 구현

--- src/main/java/company/board_project/content/controller/ContentController.java
+++ src/main/java/company/board_project/content/controller/ContentController.java
@@ -1,18 +1,24 @@
 package company.board_project.content.controller;
 
+import company.board_project.comment.repository.CommentRepository;
 import company.board_project.content.dto.ContentPatchDto;
 import company.board_project.content.dto.ContentPostDto;
+import company.board_project.content.dto.ContentResponseDto;
+import company.board_project.content.entity.Content;
 import company.board_project.content.mapper.ContentMapper;
 import company.board_project.content.service.ContentService;
+import company.board_project.response.MultiResponseDto;
 import company.board_project.response.SingleResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @Validated
@@ -20,43 +26,63 @@ import javax.validation.constraints.Positive;
 @RequestMapping("/contents")
 @RequiredArgsConstructor
 public class ContentController {
-//    private final ContentService contentService;
-//    private final ContentMapper contentMapper;
+    private final ContentService contentService;
+    private final ContentMapper contentMapper;
+    private final CommentRepository commentRepository;
+
 
     // 게시글 생성 //
     @PostMapping
     public ResponseEntity postContent(@RequestBody ContentPostDto requestBody) {
-        return null;
-//        return new ResponseEntity<>( new SingleResponseDto<>(, HttpStatus.CREATED));
+
+        Content content = contentService.createContent(contentMapper.contentPostDtoToContent(requestBody));
+        ContentResponseDto contentResponse = contentMapper.contentToContentResponse(content);
+
+        return new ResponseEntity<>(
+                new SingleResponseDto<>(contentResponse) , HttpStatus.CREATED
+        );
     }
 
     // 게시글 단건 조회 //
     @GetMapping("/{contentId}")
     public ResponseEntity getContent(@PathVariable("contentId") Long contentId) {
-        return null;
-//        return new ResponseEntity<>(, HttpStatus.OK);
+        Content content = contentService.findContent(contentId);
+
+        return new ResponseEntity<>(contentMapper.contentToContentAllResponse(content, commentRepository),
+                HttpStatus.OK);
     }
 
     // 게시글 전체 조회 //
     @GetMapping
     public ResponseEntity getContents(@Positive @RequestParam("page") int page,
-                                      @Positive @RequestParam("size") int size) {
-        return null;
-//        return new ResponseEntity<>(, HttpStatus.OK);
+                                      @Positive @RequestParam("page") int size){
+
+        Page<Content> pageContents = contentService.findContents(page - 1, size);
+        List<Content> contents = pageContents.getContent();
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(contentMapper.contentsToContentsResponse(contents),
+                        pageContents),
+                HttpStatus.OK);
     }
 
     // 게시글 수정 //
     @PatchMapping("/{contentId}")
     public ResponseEntity patchContent(@RequestBody ContentPatchDto requestBody,
                                        @PathVariable("contentId") Long contentId) {
-        return null;
-//        return new ResponseEntity<>(, HttpStatus.OK);
+        requestBody.updateId(contentId);
+        Content content = contentService.updateContent(
+                contentMapper.contentPatchDtoToContent(requestBody));
+
+        ContentResponseDto contentResponse = contentMapper.contentToContentResponse(content);
+
+        return new ResponseEntity<>(contentResponse, HttpStatus.OK);
     }
 
     // 게시글 삭제 //
     @DeleteMapping("/{contentId}")
     public ResponseEntity deleteContent(@PathVariable("contentId") Long contentId) {
-        return null;
-//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        contentService.deleteContent(contentId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
