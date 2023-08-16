6f84a7210f4f4df15a77aecfef94b75e1323c9a3
kangharyeom <kangharyeom@github.com>
2023-08-16 12:05:09 +0900

    Refactor: MemberMapper 코드 개선
    - MemberResponseDto 매핑 추가

--- src/main/java/company/board_project/member/mapper/MemberMapper.java
+++ src/main/java/company/board_project/member/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package company.board_project.member.mapper;
 
 import company.board_project.member.dto.MemberPatchDto;
 import company.board_project.member.dto.MemberPostDto;
+import company.board_project.member.dto.MemberResponseDto;
 import company.board_project.member.entity.Member;
 import org.mapstruct.Mapper;
 
@@ -11,4 +12,13 @@ public interface MemberMapper {
 
     Member memberPatchDtoToMember(MemberPatchDto requestBody);
 
+    default MemberResponseDto memberToMemberResponseDto(Member member) {
+        return MemberResponseDto.builder()
+                .memberId(member.getMemberId())
+                .email(member.getEmail())
+                .name(member.getName())
+                .password(member.getPassword())
+                .phone(member.getPhone())
+                .build();
+    }
 }
