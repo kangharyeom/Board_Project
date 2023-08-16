c598dc5b42527730abdfeef5632dd475f3d85880
kangharyeom <kangharyeom@github.com>
2023-08-16 10:32:41 +0900

    Refactor: MemberRepository 코드 개선
    - findByEmail 추가
    - findAll 추가

--- src/main/java/company/board_project/member/repository/MemberRepository.java
+++ src/main/java/company/board_project/member/repository/MemberRepository.java
@@ -1,4 +1,13 @@
 package company.board_project.member.repository;
 
-public class MemberRepository {
+import company.board_project.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    List<Member> findAll(Member member);
 }
