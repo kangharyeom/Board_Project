1ec092cb9e6b0d629de0ee10a7e710a0ed36dedb
kangharyeom <kangharyeom@github.com>
2023-08-16 17:23:46 +0900

    Feat: Role enum 추가

---
+++ src/main/java/company/board_project/constant/Role.java
@@ -0,0 +1,15 @@
+package company.board_project.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+
+    ROLE_ADMIN("관리자"), ROLE_MANAGER("매니저"), ROLE_MEMBER("일반사용자");
+
+    private String description;
+
+    Role(String description) {
+        this.description = description;
+    }
+}
