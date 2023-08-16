a312341c14a5e69d7b38ba3730c086f91a0250b9
kangharyeom <kangharyeom@github.com>
2023-08-16 14:15:21 +0900

    Refactor: MemberMapper 회원 전체 조회를 위한 매핑

--- src/main/java/company/board_project/member/mapper/MemberMapper.java
+++ src/main/java/company/board_project/member/mapper/MemberMapper.java
@@ -1,10 +1,14 @@
 package company.board_project.member.mapper;
 
+import company.board_project.member.dto.MemberListDto;
 import company.board_project.member.dto.MemberPatchDto;
 import company.board_project.member.dto.MemberPostDto;
 import company.board_project.member.dto.MemberResponseDto;
 import company.board_project.member.entity.Member;
 import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -12,6 +16,7 @@ public interface MemberMapper {
 
     Member memberPatchDtoToMember(MemberPatchDto requestBody);
 
+    // 회원 단건 조회
     default MemberResponseDto memberToMemberResponseDto(Member member) {
         return MemberResponseDto.builder()
                 .memberId(member.getMemberId())
@@ -21,4 +26,26 @@ public interface MemberMapper {
                 .phone(member.getPhone())
                 .build();
     }
+
+    // 회원 전체 List
+    default MemberListDto membersToMemberListResponse(List<Member> memberList) {
+        return MemberListDto.builder()
+                .memberResponseDto(membersToMembersResponse(memberList))
+                .build();
+                
+    }
+
+    // 회원 response 리스트화
+    default List<MemberResponseDto> membersToMembersResponse(List<Member> members){
+        return members.stream()
+                .map(member -> MemberResponseDto.builder()
+                        .memberId(member.getMemberId())
+                        .email(member.getEmail())
+                        .name(member.getName())
+                        .password(member.getPassword())
+                        .phone(member.getPhone())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
 }
