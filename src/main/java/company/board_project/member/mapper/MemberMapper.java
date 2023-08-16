761ce47f81e2499290e95fbbca705be2598982d0
kangharyeom <kangharyeom@github.com>
2023-08-16 10:32:01 +0900

    Refactor: MemberMapper 코드 개선
    - memberPostDtoToMember 추가
    - memberPatchDtoToMember 추가

--- src/main/java/company/board_project/member/mapper/MemberMapper.java
+++ src/main/java/company/board_project/member/mapper/MemberMapper.java
@@ -1,4 +1,14 @@
 package company.board_project.member.mapper;
 
-public class MemberMapper {
+import company.board_project.member.dto.MemberPatchDto;
+import company.board_project.member.dto.MemberPostDto;
+import company.board_project.member.entity.Member;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface MemberMapper {
+    Member memberPostDtoToMember(MemberPostDto requestBody);
+
+    Member memberPatchDtoToMember(MemberPatchDto requestBody);
+
 }
