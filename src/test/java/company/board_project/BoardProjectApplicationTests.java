cd43557859ccfdfbb80adeea2e61021caf73e1a9
kangharyeom <kangharyeom@github.com>
2023-08-17 17:07:54 +0900

    Refactor: Test 미사용으로 주석처리

--- src/test/java/company/board_project/BoardProjectApplicationTests.java
+++ src/test/java/company/board_project/BoardProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package company.board_project;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BoardProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package company.board_project;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BoardProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
