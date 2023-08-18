4dd9ae9efb32c392c2827265a2a66aed4ebfbe52
kangharyeom <kangharyeom@github.com>
2023-08-18 11:04:18 +0900

    Refactor: setter 삭제

--- src/main/java/company/board_project/user/dto/UserPostDto.java
+++ src/main/java/company/board_project/user/dto/UserPostDto.java
@@ -3,7 +3,6 @@ package company.board_project.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
