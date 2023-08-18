ab9dd5224bd230beb60f4ffcc5aab587c26a1fbf
kangharyeom <kangharyeom@github.com>
2023-08-18 17:04:31 +0900

    Feat: SessionLogin 구현

---
+++ src/main/java/company/board_project/auth/login/controller/LoginControllerV2.java
@@ -0,0 +1,98 @@
+/*
+package company.board_project.auth.login.controller;
+
+import company.board_project.auth.login.SessionManager;
+import company.board_project.auth.login.dto.LoginForm;
+import company.board_project.auth.login.service.LoginService;
+import company.board_project.constant.SessionConst;
+import company.board_project.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginControllerV2 {
+
+    private final LoginService loginService;
+    // 세션
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginForm";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        User loginUser = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginUser == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠기(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("userId", String.valueOf(loginUser.getUserId()));
+        response.addCookie(idCookie);
+        return "redirect:/";
+
+    }
+
+
+    // 서블릿 HTTP 세션 사용
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        User loginUser = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginUser == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성해서 반환
+        // getSession() : 디폴트가 true, false는 세션이 없을 때 새로 만들지 않고 null을 반환
+        HttpSession session = request.getSession();
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(String.valueOf(SessionConst.LOGIN_MEMBER), loginUser);
+
+        return "redirect:/";
+    }
+
+    // 서블릿 HTTP 세션 사용
+    @PostMapping("/users/logout")
+    public String logoutV3(HttpServletRequest request) {
+        // getSession(false) 를 사용해야 함 (세션이 없더라도 새로 생성하면 안되기 때문)
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+}*/
