2135bf651280c17cf5c6b7f48fa470bd8385754e
kangharyeom <kangharyeom@github.com>
2023-08-21 17:16:38 +0900

    Refactor: HomeController 수정

--- src/main/java/company/board_project/board/HomeController.java
+++ src/main/java/company/board_project/board/HomeController.java
@@ -5,15 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 @Slf4j
 @Controller
@@ -40,5 +35,20 @@ public class HomeController {
     @RequestMapping("/join")
     public String join(){
         return "join";
+    }
+
+    @RequestMapping("/users")
+    public String users(){
+        return "users";
+    }
+
+    @RequestMapping("/board")
+    public String board(){
+        return "board";
+    }
+
+    @RequestMapping("/board/post")
+    public String boardPost(){
+        return "/board/boardPost";
     }
 }
