a3e191e6c442df186fe680474467c9806ad52942
kangharyeom <kangharyeom@gmail.com>
2023-09-26 13:09:11 +0900

    Refactor: jwt 추가

--- src/main/java/company/board_project/config/SecurityConfiguration.java
+++ src/main/java/company/board_project/config/SecurityConfiguration.java
@@ -1,38 +1,92 @@
-//package company.board_project.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-////@EnableWebSecurity(debug = true)
-//public class SecurityConfiguration {
-//    @Bean
-//    public SecurityFilterChain filterChainV3(HttpSecurity http) throws Exception {
-//        http
-//                .headers().frameOptions().sameOrigin() // 설정 추가된 부분
-//                .and()
-//                .csrf().disable()
-//                .formLogin()
-//                .loginPage("/auths/login-form")
-//                .loginProcessingUrl("/process_login")
-//                .failureUrl("/auths/login-form?error")
-//                .and()
-//                .logout()
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/")
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/auths/access-denied")
-//                .and()
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .antMatchers("/orders/**").hasRole("ADMIN")
-//                        .antMatchers("/members/my-page").hasRole("USER")
-//                        .antMatchers("/**").permitAll()
-//                );
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//}
+package company.board_project.config;
+
+import company.board_project.security.jwt.component.JwtTokenizer;
+import company.board_project.security.jwt.filter.JwtAuthenticationFilter;
+import company.board_project.security.jwt.filter.JwtVerificationFilter;
+import company.board_project.security.jwt.handler.*;
+import company.board_project.security.utils.CustomAuthorityUtils;
+import company.board_project.security.utils.RedisUtils;
+import company.board_project.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+    private final RedisUtils redisUtils;
+    private final UserRepository userRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin()
+                .and()
+                .csrf().disable()
+                .cors()
+                .and()
+                .formLogin()
+                .loginPage("/oauth2login")// 인증이 필요한 URL에 접근하면 /loginForm으로 이동
+                .usernameParameter("email")		// 로그인 시 form에서 가져올 값(id, email 등이 해당)
+                .passwordParameter("password")		// 로그인 시 form에서 가져올 값
+                .loginProcessingUrl("/social")		// 로그인을 처리할 URL 입력
+                .defaultSuccessUrl("/")			// 로그인 성공하면 "/" 으로 이동
+                .failureUrl("/oauth2login")		//로그인 실패 시 /loginForm으로 이동
+                .and()
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new UserAuthenticationEntryPoint())
+                .accessDeniedHandler(new UserAccessDeniedHandler())
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .antMatchers(HttpMethod.GET, "/**").permitAll()
+                        .antMatchers(HttpMethod.POST, "/user/join","/auth/email","/auth/password").permitAll()
+                        .antMatchers(HttpMethod.PATCH, "/auth/password").permitAll()
+                        .antMatchers(HttpMethod.DELETE, "/user").hasRole("USER")
+                        .antMatchers("/h2/**").permitAll()
+                        .antMatchers(HttpMethod.OPTIONS).permitAll()
+                        .antMatchers("/oauth2/**", "/loading/**","/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(new OAuth2UserSuccessHandler(jwtTokenizer, userRepository,redisUtils)));
+
+        return http.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @CrossOrigin
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer, redisUtils);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new UserAuthenticationSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new UserAuthenticationFailureHandler());
+
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils, redisUtils);
+
+            builder.addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+        }
+    }
+}
