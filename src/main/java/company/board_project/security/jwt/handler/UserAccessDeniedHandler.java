68bcb25ab1693422b7da15ce460953a5b18a6c86
kangharyeom <kangharyeom@gmail.com>
2023-09-26 13:11:11 +0900

    Refactor: auth 추가

---
+++ src/main/java/company/board_project/security/jwt/handler/UserAccessDeniedHandler.java
@@ -0,0 +1,24 @@
+package company.board_project.security.jwt.handler;
+
+import company.board_project.exception.ErrorResponder;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class UserAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        ErrorResponder.sendErrorResponse(response, HttpStatus.FORBIDDEN);
+//        log.warn("해당 리소스에 대한 권한 없음 : {}", accessDeniedException.getMessage());
+    }
+}
