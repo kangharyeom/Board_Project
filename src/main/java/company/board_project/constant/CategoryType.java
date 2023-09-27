65fd224757dd5f46dd3370c1e8659f7240bb0771
kangharyeom <kangharyeom@gmail.com>
2023-09-27 15:05:11 +0900

    Refactor: category 최신화

--- src/main/java/company/board_project/constant/CategoryType.java
+++ src/main/java/company/board_project/constant/CategoryType.java
@@ -1,5 +1,5 @@
 package company.board_project.constant;
 
 public enum CategoryType {
-    MATCH, LEAGUE, RANKING, TEAM, BOARD, FREE, STATIC, RECRUIT, TEAM_MANAGEMENT
+    MATCH, LEAGUE, RANKING, TEAM, BOARD, FREE, STATIC, RECRUIT, TEAM_MANAGEMENT, IMAGE
 }
