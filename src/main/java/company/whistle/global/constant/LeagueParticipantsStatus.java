fe36b35f2d76054526b0d0d4e1771d346cdf0a90
kangharyeom <kangharyeom@gmail.com>
2023-12-07 09:50:11 +0900

    Feat: enum 추가

---
+++ src/main/java/company/whistle/global/constant/LeagueParticipantsStatus.java
@@ -0,0 +1,9 @@
+package company.whistle.global.constant;
+
+public enum LeagueParticipantsStatus {
+    ACTIVITY,
+    INACTIVITY,
+    QUIT,
+    BAN,
+    LEAGUE_END
+}
---
+++ src/main/java/company/whistle/global/constant/TeamMemberStatus.java
@@ -0,0 +1,8 @@
+package company.whistle.global.constant;
+
+public enum TeamMemberStatus {
+    ACTIVITY,
+    INACTIVITY,
+    QUIT,
+    BAN
+}
